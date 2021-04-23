a4cfc18f7c8f9b7e660ea42713407223989c27b9
pyae soan aung <server01.psa1981@gmail.com>
2021-04-23 08:15:56 +0630

    linear regression

---
+++ 05 Linear regression/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+bmi_life_data = pd.read_csv("bmi_and_life_expectancy.csv")
+
+model = LinearRegression()
+model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
+
+laos_life_exp = model.predict([[21.07931]])
+
+print(laos_life_exp)
